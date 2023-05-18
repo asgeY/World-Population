59cc2c288e48125c0a0d5160a4b867c675ebe783
AsgeY <asmow@yahoo.com>
2023-05-17 21:35:03 -0500

    The World Population app provides real-time updates on the current global population

--- World Population/ContentView.swift
+++ World Population/ContentView.swift
@@ -7,20 +7,53 @@
 
 import SwiftUI
 
+
 struct ContentView: View {
+    @StateObject private var viewModel: WorldPopulationViewModel
+    
+    init() {
+        _viewModel = StateObject(wrappedValue: WorldPopulationViewModel())
+    }
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        ZStack {
+            LinearGradient(
+                gradient: Gradient(colors: [Color.yellow, Color.green]),
+                startPoint: .top,
+                endPoint: .bottom
+            )
+            .edgesIgnoringSafeArea(.all)
+            
+            VStack {
+                
+                Text("World Population")
+                    .font(.largeTitle)
+                    .padding()
+                
+                Spacer()
+                
+                Text(viewModel.populationCount)
+                    .font(.title)
+                    .padding()
+                
+                if let apiStatus = viewModel.apiStatus {
+                    Text(apiStatus)
+                        .foregroundColor(apiStatus == "Error" ? .red : .green)
+                        .padding()
+                    
+                }
+                Spacer()
+            }
         }
-        .padding()
     }
 }
+
+
+
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
     }
 }
+
---
+++ World Population/WorldPopulationViewModel.swift
@@ -0,0 +1,78 @@
+//
+//  WorldPopulationViewModel.swift
+//  World Population
+//
+//  Created by AsgeY on 5/17/23.
+//
+
+import SwiftUI
+import Combine
+import Alamofire
+
+struct Config {
+    static let apiKey = "ee00d10908msh796ef7b6fc59ea3p18cc0fjsn1748c7c859e1"
+}
+
+
+class WorldPopulationViewModel: ObservableObject {
+    @Published var populationCount: String = ""
+    @Published var apiStatus: String?
+    
+    private var cancellables = Set<AnyCancellable>()
+    private var request: DataRequest?
+    
+    init() {
+        startPolling()
+    }
+    
+    func startPolling() {
+        fetchWorldPopulation() // Fetch immediately upon starting
+        
+        // Schedule periodic fetches
+        Timer.publish(every: 0.1, on: .main, in: .common)
+            .autoconnect()
+            .sink { [weak self] _ in
+                self?.fetchWorldPopulation()
+            }
+            .store(in: &cancellables)
+    }
+    
+    func fetchWorldPopulation() {
+        establishConnection()
+        
+        request?.responseDecodable(of: PopulationResponse.self) { [weak self] response in
+            switch response.result {
+            case .success(let populationResponse):
+                let readableFormat = populationResponse.readableFormat
+                DispatchQueue.main.async {
+                    self?.populationCount = readableFormat
+                
+                }
+            case .failure(let error):
+                DispatchQueue.main.async {
+                    self?.apiStatus = "Error: \(error.localizedDescription)"
+                }
+            }
+        }
+    }
+    
+    private func establishConnection() {
+        let url = URL(string: "https://get-population.p.rapidapi.com/population")!
+        
+        let headers: HTTPHeaders = [
+            "X-RapidAPI-Key": Config.apiKey,
+            "X-RapidAPI-Host": "get-population.p.rapidapi.com"
+        ]
+        
+        request = AF.request(url, headers: headers)
+    }
+}
+struct PopulationResponse: Codable {
+    let count: Int
+    let readableFormat: String
+
+    enum CodingKeys: String, CodingKey {
+        case count
+        case readableFormat = "readable_format"
+    }
+}
